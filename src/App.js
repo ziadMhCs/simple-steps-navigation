60fdd463eabccc150557771e13d7108cd98e4db9
ziad <ziadoLearningGIT@mindyourownbusniss.com>
2024-10-30 01:10:04 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App(){
+//  var step =1;
+const [step,setStep] = useState(1);
+const [isOpened,setIsOpend] = useState(true);
+function handelNext(){
+ if (step<3)
+   setStep(s=>s+1);
+} 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ function handelPrevious(){
+if (step>1)
+    setStep(step-1);
+  
+
+ }
+
+
+return(
+<>
+<button onClick={()=>  { setIsOpend(!isOpened)}} className="close">&times;</button>
+{
+  isOpened && (
+    <div className="steps">
+    <div className="numbers">
+    <div className={step>=1 ?"active":""}>1</div>
+    <div className={step>=2 ?"active":""}>2</div>
+    <div className={step>=3 ?"active":""}>3</div>
     </div>
-  );
-}
+    <p className="message"> step {step}: {messages[step-1]} </p>
+    <div className="buttons">
 
-export default App;
+  <button  onClick={handelPrevious} style={{backgroundColor:"#7950f2", color:"#fff"}}>previous</button>
+  <button   onClick={handelNext} style={{backgroundColor:"#7950f2", color:"#fff"}}>next</button>
+    </div>
+
+  </div>
+)
+}
+</>)
+}
